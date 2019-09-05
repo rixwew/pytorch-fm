3e41b4fe1dfcd9e768af02b6a8365fe46de2df78
bairan <qaq.febr2.qaq@gmail.com>
2019-09-05 17:02:07 +0800

    Update pnn.py
    Update the PNN structure based on the PNN's extend version: https://arxiv.org/pdf/1807.00311.pdf
    which said "The n embeddings of v, and the n(n − 1)/2 interactions of p are flattened and fully connected with the successive hidden layer"

--- torchfm/model/pnn.py
+++ torchfm/model/pnn.py
@@ -7,7 +7,6 @@ from torchfm.layer import FeaturesEmbedding, FeaturesLinear, InnerProductNetwork
 class ProductNeuralNetworkModel(torch.nn.Module):
     """
     A pytorch implementation of inner/outer Product Neural Network.
-
     Reference:
         Y Qu, et al. Product-based Neural Networks for User Response Prediction, 2016.
     """
@@ -15,52 +14,23 @@ class ProductNeuralNetworkModel(torch.nn.Module):
     def __init__(self, field_dims, embed_dim, mlp_dims, dropout, method='inner'):
         super().__init__()
         num_fields = len(field_dims)
-        self.embed_output_dim = num_fields * embed_dim
-        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
-
-        # linear_signals_weights
-        self.linear_signals_weights = torch.nn.ModuleList([
-            torch.nn.ParameterList([
-                torch.nn.Parameter(torch.randn(embed_dim), requires_grad=True) for _ in range(num_fields)
-            ]) for _ in range(mlp_dims[0])
-        ])
-
-        # quadratic_signals_weights
-        if method == "inner":
-            self.quadratic_signals_weights = torch.nn.ModuleList([
-                InnerProductNetwork() for _ in range(mlp_dims[0])
-            ])
-        elif method == "outer": 
-            self.quadratic_signals_weights = torch.nn.ModuleList([
-                OuterProductNetwork(num_fields, embed_dim) for _ in range(mlp_dims[0])
-            ])
+        if method == 'inner':
+            self.pn = InnerProductNetwork()
+        elif method == 'outer':
+            self.pn = OuterProductNetwork(num_fields, embed_dim)
         else:
-            raise ValueError('unknow product type: ' + method) 
-
-        # MLP layer
-        self.bias = torch.nn.Parameter(torch.randn(mlp_dims[0]), requires_grad=True)
-        self.mlp = MultiLayerPerceptron(mlp_dims[0], mlp_dims[1:], dropout)
+            raise ValueError('unknown product type: ' + method)
+        self.embedding = FeaturesEmbedding(field_dims, embed_dim)
+        self.linear = FeaturesLinear(field_dims, embed_dim)
+        self.embed_output_dim = num_fields * embed_dim
+        self.mlp = MultiLayerPerceptron(num_fields * (num_fields - 1) // 2 + self.embed_output_dim, mlp_dims, dropout)
 
     def forward(self, x):
         """
         :param x: Long tensor of size ``(batch_size, num_fields)``
         """
         embed_x = self.embedding(x)
-        
-        linear_signals_arr = []
-        for _, weight_layer in enumerate(self.linear_signals_weights):
-            tmp_arr = []
-            for j, weight_field in enumerate(weight_layer):
-                tmp_arr.append(torch.sum(embed_x[:,j]*weight_field, 1))
-            linear_signals_arr.append(sum(tmp_arr).view([-1, 1]))
-        linear_signals = torch.cat(linear_signals_arr, 1)
-
-        quadratic_signals_arr = []
-        for pn in self.quadratic_signals_weights:
-            tmp_pn = pn(embed_x)
-            quadratic_signals_arr.append(tmp_pn)
-        quadratic_signals = torch.cat(quadratic_signals_arr,1)
-
-        deep_x = linear_signals + quadratic_signals + self.bias
-        deep_x = self.mlp(deep_x)
-        return torch.sigmoid(deep_x.squeeze(1))
+        cross_term = self.pn(embed_x)
+        x = torch.cat([embed_x.view(-1, self.embed_output_dim), cross_term], dim=1)
+        x = self.mlp(x)
+        return torch.sigmoid(x.squeeze(1))
